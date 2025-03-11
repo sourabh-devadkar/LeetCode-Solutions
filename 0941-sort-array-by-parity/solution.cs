0f98c11be4e64e95cce738e0b398de7f9e3a7ec0
Sourabh Shashikant Devadkar <91150846+sourabh-devadkar@users.noreply.github.com>
2025-03-11 17:02:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 51.1 MB (86.21%)

---
+++ 0941-sort-array-by-parity/solution.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] SortArrayByParity(int[] nums) {
+        int i=0, j=nums.Length-1;
+        while(i<j)
+        {
+            if(nums[i]%2 == 1){
+                if(nums[j]%2 == 0){
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    i++;
+                }
+                j--;
+            }else{
+                i++;
+            }   
+        }
+        return nums;
+    }
+}
