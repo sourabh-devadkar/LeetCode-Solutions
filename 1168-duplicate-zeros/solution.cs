b8b36440472a7b79b6f066feeef3f95a23183b52
Sourabh Shashikant Devadkar <91150846+sourabh-devadkar@users.noreply.github.com>
2025-12-26 13:29:59 +0000

    Sync LeetCode submission Runtime - 9 ms (33.93%), Memory - 49 MB (7.14%)

--- 1168-duplicate-zeros/solution.cs
+++ 1168-duplicate-zeros/solution.cs
@@ -1,13 +1,33 @@
 public class Solution {
     public void DuplicateZeros(int[] arr) {
-        for(int i=0; i< arr.Length; i++){
-            if(arr[i] == 0){
-                
-                for(int j = arr.Length - 1; j > i; j--){
-                   arr[j] = arr[j-1];
+        int i = 0;
+        while(i < arr.Length)
+        {
+            if(arr[i] == 0 && i < arr.Length - 1)
+            {
+                for(int j = arr.Length - 1; j > i + 1; j--)
+                {
+                    arr[j] = arr[j - 1];
                 }
+                arr[i + 1] = 0;
                 i++;
             }
+            i++;
         }
+        // int zeroCount = 0;
+        // int index = 0;
+        // for(int i = 0; i < arr.Length; i++)
+        // {
+        //     zeroCount++;
+        //     if(arr[i] == 0)
+        //         zeroCount++;
+        //     if(zeroCount >= arr.Length)
+        //         index = i;
+        // }
+        // int length = arr.Length - 1;
+        // while(index >= 0)
+        // {
+        //     arr[]
+        // }
     }
 }
