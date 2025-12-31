98b83f3a263dfa5f52dd8e7fdf805f6211dc216e
Sourabh Shashikant Devadkar <91150846+sourabh-devadkar@users.noreply.github.com>
2025-12-31 12:13:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 47.2 MB (14.03%)

--- 0027-remove-element/solution.cs
+++ 0027-remove-element/solution.cs
@@ -1,30 +1,14 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
-        int n = nums.Length;
-        if(n == 0) return 0;
-        int count = 0;
-        int i=0, j = n-1;
-        bool flag = false;
-        while(i < j && i < n){
-            if(nums[i] == val){
-                if(nums[j] != val){
-                    nums[i] = nums[j];
-                    nums[j] = val;
-                    i++;
-                    flag = true;
-                }
-                j--;
-            }else{
+        int i = 0;
+        for(int j = 0; j < nums.Length; j++)
+        {
+            if(nums[j] != val)
+            {
+                nums[i] = nums[j];
                 i++;
             }
         }
-        for(int k=0; k<n; k++){
-            if(nums[k] == val){
-                break;
-            }else{
-                count++;
-            }
-        }
-        return count;
+        return i;
     }
 }
