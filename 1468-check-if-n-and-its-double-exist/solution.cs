ffe32144b7385d84b68322d28a9929656f26baf5
Sourabh Shashikant Devadkar <91150846+sourabh-devadkar@users.noreply.github.com>
2025-12-31 12:30:57 +0000

    Sync LeetCode submission Runtime - 2 ms (73.11%), Memory - 44.5 MB (37.82%)

--- 1468-check-if-n-and-its-double-exist/solution.cs
+++ 1468-check-if-n-and-its-double-exist/solution.cs
@@ -1,13 +1,13 @@
 public class Solution {
     public bool CheckIfExist(int[] arr) {
-        int n = arr.Length;
-        for(int i =0; i<n; i++){
-            for (int j =0; j< n; j++){
-                if(i != j){
-                    if(arr[i] == 2 * arr[j] || arr[i] * 2 == arr[j]){
-                        return true;
-                    }
-                }
+        for(int i = 0; i < arr.Length; i++)
+        {
+            for(int j = i + 1; j < arr.Length; j++)
+            {
+                if(arr[i] == arr[j] * 2)
+                    return true;
+                if(arr[j] % 2 == 0 && arr[j] / 2 == arr[i])
+                    return true;
             }
         }
         return false;
