93552c2d36318f0cdc09172610744bdea5b24247
Sourabh Shashikant Devadkar <91150846+sourabh-devadkar@users.noreply.github.com>
2026-04-13 16:56:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 50.1 MB (11.33%)

---
+++ 0167-two-sum-ii---input-array-is-sorted/solution.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.Length - 1;
+        int[] result = new int[2];
+        while(i < j)
+        {
+            if(numbers[i] + numbers[j] == target)
+            {
+                result[0] = i+1;
+                result[1] = j+1;
+                break;
+            } else if(numbers[i] + numbers[j] > target)
+            {
+                j--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return result;
+    }
+}
