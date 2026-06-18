7b7709ef5326f6b6b0f764ff5dbb9b2923bc662d
Sourabh Shashikant Devadkar <91150846+sourabh-devadkar@users.noreply.github.com>
2026-06-18 17:16:22 +0000

    Sync LeetCode submission Runtime - 277 ms (25.16%), Memory - 0.0B (100.00%)

--- 0584-find-customer-referee/solution.sql
+++ 0584-find-customer-referee/solution.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
-select name from Customer where referee_id is null or referee_id != 2;
+-- Write your PostgreSQL query statement below
+SELECT name FROM Customer WHERE referee_id IS NULL OR referee_id != 2;
