eb46058315e2cc4f4026bbf7c3d10c44e32a0115
Sourabh Shashikant Devadkar <91150846+sourabh-devadkar@users.noreply.github.com>
2026-06-24 17:10:34 +0000

    Sync LeetCode submission Runtime - 2 ms (83.19%), Memory - 41.4 MB (76.11%)

--- 1137-height-checker/solution.cs
+++ 1137-height-checker/solution.cs
@@ -1,19 +1,31 @@
 public class Solution {
     public int HeightChecker(int[] heights) {
-        int[] expectedHeights = new int[heights.Length];
-        Array.Copy(heights, 0, expectedHeights, 0, heights.Length);
-        expectedHeights = Sort(expectedHeights);
-        int counter = 0;
+        int[] sortedHeights = Sort(heights);
+        int count = 0;
         for(int i = 0; i < heights.Length; i++)
         {
-            if(heights[i] != expectedHeights[i])
-                counter++;
+            if(heights[i] != sortedHeights[i])
+                count++;
         }
-        return counter;
+        return count;
     }
+    
     public int[] Sort(int[] heights)
     {
-        Array.Sort(heights);
-        return heights;
+        int[] newArr = new int[heights.Length];
+        Array.Copy(heights, 0, newArr, 0, heights.Length);
+        for(int i=0; i< newArr.Length - 1; i++)
+        {
+            for(int j=0; j<newArr.Length - 1; j++)
+            {
+                if(newArr[j] > newArr[j+1])
+                {
+                    int temp = newArr[j+1];
+                    newArr[j+1] = newArr[j];
+                    newArr[j] = temp;
+                }
+            }
+        }
+        return newArr;
     }
 }
