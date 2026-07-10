8f875c1f130adce51e8e7665cfced9d6ea9c6041
Sourabh Devadkar <91150846+sourabh-devadkar@users.noreply.github.com>
2026-07-10 23:18:51 +0530

    Time: 1836 ms (5.48%), Space: 73.4 MB (38.03%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        HashSet<int> uniqueSet = new HashSet<int>(nums);
+        int maxLength = 0;
+        foreach(int num in nums)
+        {
+            if(uniqueSet.Contains(num - 1)) 
+                continue;
+
+            int length = 0;
+            while(uniqueSet.Contains(num + length))
+            {
+                length++;
+            }
+
+            if(length > maxLength)
+                maxLength = length;
+        }
+
+        return maxLength;
+    }
+}
